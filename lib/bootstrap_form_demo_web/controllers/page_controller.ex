0590a5210fc20e49c2d49df39226bb8ecc246064
Felipe Renan <felipe.renan@plataformatec.com.br>
2019-04-19 16:22:11 -0300

    Create the first form-example

--- lib/bootstrap_form_demo_web/controllers/page_controller.ex
+++ lib/bootstrap_form_demo_web/controllers/page_controller.ex
@@ -1,7 +1,21 @@
 defmodule BootstrapFormDemoWeb.PageController do
   use BootstrapFormDemoWeb, :controller
 
+  alias BootstrapFormDemo.Accounts
+  alias BootstrapFormDemo.Accounts.User
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    changeset = Accounts.change_user(%User{})
+
+    render(conn, "index.html", changeset: changeset)
+  end
+
+  def create(conn, %{ "user" => user_params }) do
+    with {:ok, user} <- Accounts.create_user(user_params) do
+      put_flash(conn, :success, "User created with success")
+    else
+      {:error, changeset} ->
+        render(conn, "index.html", changeset: changeset)
+    end
   end
 end
--- lib/bootstrap_form_demo_web/router.ex
+++ lib/bootstrap_form_demo_web/router.ex
@@ -17,6 +17,7 @@ defmodule BootstrapFormDemoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    post "/users", PageController, :create
   end
 
   # Other scopes may use custom stacks.

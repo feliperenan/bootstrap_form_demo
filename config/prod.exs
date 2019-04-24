10c1ffb9b8b842a0706ffbe5423140c0dc73af74
Felipe Renan <felipe.renan@plataformatec.com.br>
2019-04-23 21:44:39 -0300

    Config app to be deployed on Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,11 +11,20 @@ use Mix.Config
 # before starting your production server.
 config :bootstrap_form_demo, BootstrapFormDemoWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "ex-bootstrap-form-demo.herokuapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Database configuration
+config :bootstrap_form_demo, BootstrapFormDemoWeb.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -68,4 +77,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
